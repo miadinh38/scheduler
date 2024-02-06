6758d46af1dbb676f01c55a459d2d809c03e5903
Mia Dinh <miadinh38@gmail.com>
2024-02-06 03:05:51 -0500

    Build the first custom hook called useVisualMode and run test

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const useVisualMode = (initialMode) => {
+  const [mode, setMode] = useState(initialMode);
+
+  return { mode };
+}
+
+export default useVisualMode;
