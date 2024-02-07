d937cb388561a31e8ef22ddeee6bc45f67375f4e
Mia Dinh <miadinh38@gmail.com>
2024-02-07 00:25:32 -0500

    Implement Transition with Replace and add test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,17 +4,20 @@ const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setMode(newMode);
-    setHistory(prev => [...prev, newMode]);
+  function transition(newMode, replace = false) {
+  if(replace) {
+      setMode(newMode);
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]); // Replace the last item in history with the new mode
+    } else {
+      setMode(newMode);
+      setHistory(prev => [...prev, newMode]); // Add the new mode to history
+    }
   }
-  
   function back() {
     if (history.length > 1) {
       setHistory(prev => prev.slice(0, prev.length - 1)); // Remove the last item from the history stack
       setMode(history[history.length - 2]); // Revert to the previous mode
     }
-  
   }
 
   return { mode: history[history.length -1], transition, back };
