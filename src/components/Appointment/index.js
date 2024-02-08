15962e0a9ef3ef3297d1c128b2341106e69d57b3
Mia Dinh <miadinh38@gmail.com>
2024-02-07 22:58:47 -0500

    Implement useVisualMode hook and add a transition

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
@@ -6,20 +7,30 @@ import Header from "components/Appointment/Header";
 
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import { use } from "chai";
+
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const initialMode = props.interview ? SHOW : EMPTY;
+
+  const { mode, transition, back } = useVisualMode(initialMode);
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-          onDelete={() => console.log("CONFIRM")}
-          onEdit={() => console.log("EDIT")}
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
+
     </article>
   );
 }
