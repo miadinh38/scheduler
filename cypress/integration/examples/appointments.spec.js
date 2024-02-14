b7af2a75676cfaaa0153a2a2109270726d0a54bc
Mia Dinh <miadinh38@gmail.com>
2024-02-14 04:34:28 -0500

    Uncomment code

--- cypress/integration/examples/appointments.spec.js
+++ cypress/integration/examples/appointments.spec.js
@@ -7,40 +7,40 @@ describe("appointment", () => {
     cy.contains("Monday");
   })
 
-  // it("should book an interview", () => {
+  it("should book an interview", () => {
 
-  //   //click the add button in the second appointment
-  //   cy.get("[alt=Add]")
-  //     .first()
-  //     .click();
+    //click the add button in the second appointment
+    cy.get("[alt=Add]")
+      .first()
+      .click();
     
-  //   //enter name
-  //   cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    //enter name
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
 
-  //   //choose the interviewer
-  //   cy.get("[alt='Sylvia Palmer']").click();
+    //choose the interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
 
-  //   //click the save button
-  //   cy.contains("Save").click();
+    //click the save button
+    cy.contains("Save").click();
 
-  //   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-  //   cy.contains(".appointment__card--show", "Sylvia Palmer");
-  // });
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
 
-  // it("should edit an interview", () => {
-  //   cy.get("[alt='Edit']").first().click({force: true});
+  it("should edit an interview", () => {
+    cy.get("[alt='Edit']").first().click({force: true});
 
-  //   cy.get("[data-testid=student-name-input]")
-  //     .clear()
-  //     .type("Lydia Miller-Jones");
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
     
-  //   cy.get("[alt='Tori Malcolm']").click();
+    cy.get("[alt='Tori Malcolm']").click();
 
-  //   cy.contains("Save").click();
+    cy.contains("Save").click();
 
-  //   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-  //   cy.contains(".appointment__card--show", "Tori Malcolm");
-  // });
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 
   it("should cancel an interview", () => {
     cy.get("[alt='Delete']").click( {force: true} );
